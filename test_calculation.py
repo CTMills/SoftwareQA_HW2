333db245b39b5984c99e060cf9f4001bafc8541a
CTMills <calebmills.t@hotmail.com>
2021-03-04 23:27:54 -0600

    updating syntax

--- test_calculation.py
+++ test_calculation.py
@@ -4,6 +4,7 @@ from bmi import *
 from retirement import *
 
 def test_bmi_calculate1():
+    print("\nBMI Test Case 1:")
     out = bmi_calculate(70, 250)
     if(out == 36.7):
         print("Test Passed: Expected Output = Actual Output")
@@ -12,6 +13,7 @@ def test_bmi_calculate1():
         print("Test Failed")
 
 def test_bmi_calculate2():
+    print("\nBMI Test Case 2:")
     out = bmi_calculate(50, 125)
     if(out == 36.0):
         print("Test Passed: Expected Output = Actual Output")
@@ -20,6 +22,7 @@ def test_bmi_calculate2():
         print("Test Failed")
 
 def test_bmi_calculate3():
+    print("\nBMI Test Case 3:")
     out = bmi_calculate(70, 150)
     if(out == 22.0):
         print("Test Passed: Expected Output = Actual Output")
@@ -28,6 +31,7 @@ def test_bmi_calculate3():
         print("Test Failed")
 
 def test_bmi_calculate4():
+    print("\nBMI Test Case 4:")
     out = bmi_calculate(70, 100)
     if(out == 14.7):
         print("Test Passed: Expected Output = Actual Output")
@@ -36,6 +40,7 @@ def test_bmi_calculate4():
         print("Test Failed")
 
 def test_bmi_calculate5():
+    print("\nBMI Test Case 5:")
     out = bmi_calculate(70, 175)
     assert out == 25.7
     if(out == 25.7):
@@ -45,6 +50,7 @@ def test_bmi_calculate5():
         print("Test Failed")
 
 def test_retirement_calculate1():
+    print("\nRetirement Test Case 1:")
     out = retirement_calculate(75, 255000, 15, 1000000)
     if(out == 95):
         print("Test Passed: Expected Output = Actual Output")
@@ -53,6 +59,7 @@ def test_retirement_calculate1():
         print("Test Failed")
 
 def test_retirement_calculate2():
+    print("\nRetirement Test Case 2:")
     out = retirement_calculate(25, 450000, 5, 2500000)
     if(out == 108):
         print("Test Passed: Expected Output = Actual Output")
@@ -61,6 +68,7 @@ def test_retirement_calculate2():
         print("Test Failed")
 
 def test_retirement_calculate3():
+    print("\nRetirement Test Case 3:")
     out = retirement_calculate(50, 50000, 5, 100000)
     if(out == 80):
         print("Test Passed: Expected Output = Actual Output")
@@ -69,6 +77,7 @@ def test_retirement_calculate3():
         print("Test Failed")
 
 def test_retirement_calculate4():
+    print("\nRetirement Test Case 4:")
     out = retirement_calculate(45, 500000, 35, 10000000)
     if(out == 88):
         print("Test Passed: Expected Output = Actual Output")
@@ -77,6 +86,7 @@ def test_retirement_calculate4():
         print("Test Failed")
 
 def test_retirement_calculate5():
+    print("\nRetirement Test Case 5:")
     out = retirement_calculate(50, 50000, 15, 1000000)
     if(out == 149):
         print("Test Passed: Expected Output = Actual Output")
