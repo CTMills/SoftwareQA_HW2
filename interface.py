f11b03464c67f5e1cb4d036821243da64d15d035
CTMills <56661038+CTMills@users.noreply.github.com>
2021-03-04 23:36:53 -0600

    Update interface.py

--- interface.py
+++ interface.py
@@ -9,7 +9,7 @@ from retirement import *
 def menu():
     while(1):
         print("\nMAIN MENU\nPress 1 to calculate your BMI and category\nPress 2 to calculate when you will reach your retirement goal\nPress 3 to exit the app")
-        choice = int(input("\nEnter your choice: "))
+        choice = input("\nEnter your choice: ")
 
         if (choice == 1):
             menu_output = bmi_option()
