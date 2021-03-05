16065a72950fbca0311ffbac96e0aafbafb18764
CTMills <calebmills.t@hotmail.com>
2021-03-04 23:49:00 -0600

    Final final update

--- interface.py
+++ interface.py
@@ -8,30 +8,34 @@ from retirement import *
 
 def menu():
     while(1):
-        print("\nMAIN MENU\nPress 1 to calculate your BMI and category\nPress 2 to calculate when you will reach your retirement goal\nPress 3 to exit the app")
-        choice = int(input("\nEnter your choice: "))
+        try:
+            print("\nMAIN MENU\nPress 1 to calculate your BMI and category\nPress 2 to calculate when you will reach your retirement goal\nPress 3 to exit the app")
+            choice = int(input("\nEnter your choice: "))
+                
+            if (choice == 1):
+                menu_output = bmi_option()
+                if (menu_output != -1):
+                    print(menu_output)
 
-        if (choice == 1):
-            menu_output = bmi_option()
-            if (menu_output != -1):
-                print(menu_output)
+                else:
+                    print("You made an error. Restarting...")
+
+            elif (choice == 2):
+                menu_output = retirement_option()
+                if (menu_output != -1):
+                    print(menu_output)
+
+                else:
+                    print("You made an error. Restarting...")
+
+            elif (choice == 3):
+                break
 
             else:
-                print("You made an error. Restarting...")
+                print("\nThat is not a valid choice. Restarting interface...")
 
-        elif (choice == 2):
-            menu_output = retirement_option()
-            if (menu_output != -1):
-                print(menu_output)
-
-            else:
-                print("You made an error. Restarting...")
-
-        elif (choice == 3):
-            break
-
-        else:
-            print("\nThat is not a valid choice. Restarting interface...")
+        except:
+            print("You made an error. Restarting...")
 
 def main():
     menu()
