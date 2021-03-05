d7bf64141de8cffeff6aea5d0e345d541b10a5f0
CTMills <calebmills.t@hotmail.com>
2021-03-04 23:32:32 -0600

    last update

--- retirement.py
+++ retirement.py
@@ -6,9 +6,19 @@ def retirement_option():
     age = int(input("\nPlease input your age in years: "))
     salary = int(input("\nPlease input your annual salary in dollars: "))
     savedPercent = int(input("\nPlease input percentage saved from salary: "))
-    decimalPercent = savedPercent / 100
     savingsGoal = int(input("\nPlease input your savings goal: "))
-    goalAge = math.ceil(retirement_calculate(age, salary, decimalPercent, savingsGoal))
+
+    if (age != int or salary != int or savedPercent != int or savingsGoal != int):
+        print("You must enter whole numbers")
+        error = -1
+        return error
+
+    if (age <= 0 or salary <= 0 or savedPercent <= 0 or savingsGoal <= 0):
+        print("You must not enter 0 or lower")
+        error = -1
+        return error
+
+    goalAge = retirement_calculate(age, salary, savedPercent, savingsGoal)
     if (goalAge >= 100):
         goalMet = False
 
@@ -24,7 +34,8 @@ def retirement_option():
     return phrase
     
 def retirement_calculate (age, salary, savedPercent, savingsGoal):
-    savingsPerYear = salary * savedPercent * 1.35
-    yearsTilGoal = savingsGoal / savingsPerYear
+    decimalPercent = savedPercent / 100
+    savingsPerYear = round(salary * decimalPercent * 1.35, 5)
+    yearsTilGoal = math.ceil(savingsGoal / savingsPerYear)
     goalAge = age + yearsTilGoal
     return goalAge
