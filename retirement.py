99c0cd8da6c340a1000802f0367253ebfd89792b
CTMills <calebmills.t@hotmail.com>
2021-03-04 19:13:34 -0600

    rounding retirement to whole number

--- retirement.py
+++ retirement.py
@@ -1,12 +1,14 @@
 #Caleb Mills
 #Homework 2
+import math
+
 def retirement_option():
     age = int(input("\nPlease input your age in years: "))
     salary = int(input("\nPlease input your annual salary in dollars: "))
     savedPercent = int(input("\nPlease input percentage saved from salary: "))
     decimalPercent = savedPercent / 100
     savingsGoal = int(input("\nPlease input your savings goal: "))
-    goalAge = int(retirement_calculate(age, salary, decimalPercent, savingsGoal))
+    goalAge = math.ceil(retirement_calculate(age, salary, decimalPercent, savingsGoal))
     if (goalAge >= 100):
         goalMet = False
 
